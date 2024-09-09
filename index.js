71a047f28b484cca1c03d2c41e87298c9d799049
Kully <nexus5onlykully@gmail.com>
2024-09-09 19:26:10 +0700

    init project

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var cors = require('cors')
+const mysql = require('mysql2');
+require('dotenv').config()
+var app = express()
+
+app.use(cors())
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  password: '',
+});
+
+
+const sql = 'SELECT * FROM `users`';
+
+
+app.get('/hello', function (req, res, next) {
+  res.json({msg: 'helloworld'})
+})
+
+app.get('/users', function (req, res, next) {
+  connection.query(
+    sql,
+    function (err, results, fields) {
+      res.json(results)
+    }
+  );
+  // res.json({msg: 'users'})
+})
+
+app.listen(5000, function () {
+  console.log('CORS-enabled web server listening on port 5000')
+})
